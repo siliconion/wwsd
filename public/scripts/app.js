1a88b2a89057bce15eb8e93d7dfce38fdf093260
siliconion <chihchiayang@gmail.com>
2016-10-22 16:14:07 -0500

    add initial routing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,23 @@
-var wwsd = angular.module('wwsd', [ 'ngRoute', 'ngAnimate', 'wwsdControllers' ]);
+var wwsd = angular.module('wwsd', [ 'ngRoute' ]);
+
+
+/* Initializing and configuring Templates controllers and route providers */
+wwsd.config([ '$routeProvider', function( $routeProvider ){
+  $routeProvider.when('/', {
+    templateUrl :  'templates/lobby.html',
+    controller : 'lobbyController'
+  }).when('/room', {
+    templateUrl :  'templates/room.html',
+    controller : 'roomController'
+  }).otherwise({
+    redirectTo:'/'
+  });
+}]);
+
+wwsd.controller(['lobbyController', [function(){
+  console.log("HERE!!");
+}]]);
+
+wwsd.controller(['roomController', [function(){
+  console.log("HERE!!");
+}]]);
